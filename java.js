b2e98f78a89c6afc30bc00f08b864f0076104f56
mafeermoreno <a01708653@tec.mx>
2023-02-16 09:54:19 -0600

    Agregar errores

--- java.js
+++ java.js
@@ -17,3 +17,16 @@ console.assert(1==="1");
 console.assert(1=="1");
 console.assert(1==true);
 console.assert(1===true);
+
+//Alcance de las variables
+for (let i=1; i<=10; i++){
+
+}
+//Aquí ocurre un error porque la variable "i" ya murió al terminar el ciclo
+console.log(i);
+
+for (var i=1; i<=10; i++){
+
+}
+//Aquí no hay error, la variable u sigue existiendo
+console.log(i)
