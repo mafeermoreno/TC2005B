097103f44f97b3500b041f80895f51a5997e76d6
mafeermoreno <a01708653@tec.mx>
2023-02-16 09:47:12 -0600

    Agregar variables globales js

---
+++ java.js
@@ -0,0 +1,19 @@
+//variables, constantes, consola (log, info, warn, error, assert)
+//Alcance de las variables
+
+var comida="chilaquiles";
+
+let cena="tacos";
+
+let precio=70;
+
+console.log("hola");
+console.info("valor de la comida "+comida);
+console.warn("El precio no se puede modificar");
+console.error("Te dije que el precio no se puede modifiar");
+
+console.assert(1===1);
+console.assert(1==="1");
+console.assert(1=="1");
+console.assert(1==true);
+console.assert(1===true);
