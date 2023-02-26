ea79d7e72e3afc01d4e0c06abee4979ef28ef8bb
mafeermoreno <a01708653@tec.mx>
2023-02-26 17:42:08 -0600

    Terminar ejercicio 2, string a txt

--- lab8_a01708653/lab8.app_a01708653.js
+++ lab8_a01708653/lab8.app_a01708653.js
@@ -16,17 +16,27 @@ server.listen(port, hostname, () => {
 //Ejercicio 1
 function promArreglo(arreglo1){
     let suma=0;
-    var longitud= arreglo1.length
+    var longitud= arreglo1.length;
     for (let i=0; i<arreglo1.length; i++){
         suma=(suma+arreglo1[i]);
     }
     let prom=(suma/longitud);
     if (longitud==0){
-        return prom=0
+        return prom=0;
     }
     else{
-        return prom
+        return prom;
     }
 }
 console.log("Promedio del arreglo: ");
 console.log(promArreglo([200,4923,235,4425,200,2345]))
+
+//Ejercicio 2
+function string2txt(string1){
+    //fs es el módulo para trabajar con el sistema de archivos
+    const filesystem=require('fs');
+    //Función síncrona, si es asíncrona, recibe otro parámetro con un apuntador
+    filesystem.writeFileSync('Ejercicio2.txt', string1);
+}
+string2txt("Me gusta mucho escribir en Node js")
+console.log("Terminamos de escribir el archivo");
