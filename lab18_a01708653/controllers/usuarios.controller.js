b070f5bf68b3669cf1e0baed24e0a1f210a6f3bf
mafeermoreno <a01708653@tec.mx>
2023-03-13 10:59:04 -0600

    Crear login y signup

--- lab18_a01708653/controllers/usuarios.controller.js
+++ lab18_a01708653/controllers/usuarios.controller.js
@@ -1,3 +1,31 @@
+const Usuario = require('../models/usuarios.model');
+
+exports.get_login = (request, response, next) => {
+    response.render('login');
+};
+
+exports.post_login = (request, response, next) => {
+    response.redirect('/perros');
+};
+
+
+exports.get_signup = (request, response, next) => {
+    response.render('signup');
+};
+
+exports.post_signup = (request, response, next) => {
+    const usuario = new Usuario({
+        nombre: request.body.nombre,
+        username: request.body.username,
+        password: request.body.password,
+    });
+
+    usuario.save()
+    .then(([rows, fieldData]) => {
+        response.redirect('/usuarios/login');
+    }).catch((error) => {console.log(error)});
+};
+
 exports.logout = (request, response, next) => {
     request.session.destroy(() => {
         response.redirect('/perros'); //Este código se ejecuta cuando la sesión se elimina.
---
+++ lab18_a01708653/models/usuarios.model.js
@@ -0,0 +1,23 @@
+const db = require('../util/database');
+const bcrypt = require('bcryptjs');
+
+module.exports = class Usuario {
+
+    constructor(nuevo_usuario) {
+        this.nombre = nuevo_usuario.nombre || 'John Doe';
+        this.username = nuevo_usuario.username || 'johndoe';
+        this.password = nuevo_usuario.password || 'johndoejohndoe';
+    }
+
+    save() {
+        return bcrypt.hash(this.password, 12)
+        .then((password_cifrado) => {
+            return db.execute(`
+                INSERT INTO usuarios (nombre, username, password)
+            values (?, ?, ?)
+            `, [this.nombre, this.username, password_cifrado]);
+        })
+        .catch((error) => {console.log(error)});
+    }
+
+}
