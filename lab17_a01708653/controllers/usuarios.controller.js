e87aafe98e7c11e3f6c3bd464c6b15c2b4954cb6
mafeermoreno <105958620+mafeermoreno@users.noreply.github.com>
2023-03-20 21:36:07 -0600

    Delete lab17_a01708653 directory

--- lab17_a01708653/controllers/usuarios.controller.js
+++
@@ -1,5 +0,0 @@
-exports.logout = (request, response, next) => {
-    request.session.destroy(() => {
-        response.redirect('/perros'); //Este código se ejecuta cuando la sesión se elimina.
-    });
-};
